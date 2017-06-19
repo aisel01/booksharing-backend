6e170748afe80b38ad7b10bee12f8573c3249db3
Alexandr Shleyko <dusty@yandex-team.ru>
2017-06-20 00:37:39 +0300

    добавлен пароль для пользователя

--- api/user.put.php
+++ api/user.put.php
@@ -3,24 +3,39 @@
 require_once('settings.php');
 require_once('functions.php');
 
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-$name = isset($_GET['name']) ? $_GET['name'] : null;
+$username = isset($_GET['username']) ? $_GET['username'] : null;
+$password = isset($_GET['password']) ? $_GET['password'] : null;
 
-if ($name !== null) {
-    
-    $query_user = "SELECT * FROM `users` WHERE `id` = '{$id}'";
-    $result = mysqli_query($link, $query_user);
-    
+$password = md5($password); // TODO солить пароли
+
+if ($username !== null) {
     if ($row = mysqli_fetch_assoc($result)) {
-        $query = "UPDATE `users` SET `username` = '{$name}' WHERE `id` = '{$id}'";
+        $query = "UPDATE `users` SET `password` = '{$password}' WHERE `username` = '{$username}'";
         $result = mysqli_query($link, $query);
         
-        echo json_encode('Пользователь отредактирован!');
+        echo response200([
+            'message' => 'Пользователь отредактирован!',
+            'password' => $password
+        ]);
     } else {
-        $query = "INSERT INTO `users` (`id`, `username`) VALUES (NULL, '{$name}');";
-        $result = mysqli_query($link, $query);
+        $query_user = "SELECT * FROM `users` WHERE `username` = '{$username}'";
+        $result = mysqli_query($link, $query_user);
         
-        echo json_encode('Пользователь добавлен!');
+        if ($row = mysql_fetch_row($result)) {
+            e404('Такой пользователь уже существует');
+        } else {
+            $query = "INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, '{$username}', '{$password}');";
+            $result = mysqli_query($link, $query);
+
+            if ($result) {
+                response200([
+                    'id' => mysqli_insert_id($link),
+                    'password' => $password
+                ]);
+            } else {
+                e404('Ошибка БД');
+            }
+        }
     }
 } else {
     e404('Данные не переданы!');
