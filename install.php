3be0962842a1d77566d0718fd68baff66016ba4e
aisel01 <aiselsherfedinova@yandex.ru>
2017-06-25 20:37:52 +0300

    добавлен файл-установщик для записи параметров БД

---
+++ install.php
@@ -0,0 +1,18 @@
+<?php
+echo "Host: ";
+$host = trim(fgets(STDIN));
+
+echo "User: ";
+$user = trim(fgets(STDIN));
+
+echo "Password: ";
+$password = trim(fgets(STDIN));
+
+echo "Database: ";
+$database = trim(fgets(STDIN));
+
+$text = "<?php
+	\$link = mysqli_connect('$host', '$user', '$password', '$database');";
+
+$fp = fopen("api\settings.php", "w");
+fwrite($fp, $text);
