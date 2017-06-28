f3f4ceb28d9c48bc21f5a914f39cbec8b4c8d5c5
Alexandr Shleyko <dusty@yandex-team.ru>
2017-06-28 17:06:45 +0300

    поправлен лишний md5 при логине

--- api/user.login.php
+++ api/user.login.php
@@ -1,20 +1,17 @@
 <?php
-
 require_once('settings.php');
 require_once('functions.php');
 
 $username = isset($_GET['username']) ? $_GET['username'] : null;
 $password = isset($_GET['password']) ? $_GET['password'] : null;
 
-$password = md5($password); // TODO солить пароли
-
-if (($name !== null)&&($password !== null)) {
+if (($username !== null)&&($password !== null)) {
     if (checkAuth($username, $password)) {
         $password = md5($password);
 
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
-        respose200([
+        response200([
             'password' => $password
         ]);
     } else {
